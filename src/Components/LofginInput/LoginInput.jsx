03a69912e27e7c5543cb17cb022680dbc3c0bfc7
Hamzaali565 <ha6153806@gmail.com>
2024-05-28 18:43:14 +0500

    working on routing

--- src/Components/LofginInput/LoginInput.jsx
+++ src/Components/LofginInput/LoginInput.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const LoginInput = ({ placeholder, icon }) => {
+const LoginInput = ({ placeholder, icon, onChange, type, value }) => {
   return (
     <div className="grid grid-cols-[1fr,auto] border-2 border-gray-500 rounded-2xl w-full p-3">
       <input
-        type="text"
+        type={type}
         placeholder={placeholder}
         className="outline-none bg-transparent"
+        onChange={onChange}
+        value={value}
       />
       <i class={`${icon} text-2xl`}></i>
     </div>
--- src/Screens/Login/Login.jsx
+++ src/Screens/Login/Login.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginInput from "../../Components/LofginInput/LoginInput";
 import "./Login.css";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import SimpleButton from "../../Components/Button/SimpleButton";
 const Login = () => {
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const url = useSelector((state) => state.url);
+
+  const loginUser = async () => {
+    try {
+      const response = await axios.post(`${url}/login`, {
+        userId,
+        password,
+      });
+      console.log("response of login", response);
+      setPassword("");
+      setUserId("");
+    } catch (error) {
+      console.log("error of login response", error);
+    }
+  };
+
   return (
     <div className="h-screen grid place-items-center sm: same">
       <div className="grid gap-y-8 w-full  max-w-md mx-auto p-2 bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-30 shadow-lg rounded-3xl">
@@ -15,8 +37,24 @@ const Login = () => {
           Welcome Back!
         </h1>
         <div className="grid gap-4 gap-x-3">
-          <LoginInput placeholder={"User ID "} icon={"bi bi-person-circle"} />
-          <LoginInput placeholder={"Password "} icon={"bi bi-eye-slash"} />
+          <LoginInput
+            placeholder={"User ID "}
+            icon={"bi bi-person-circle"}
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+          />
+          <LoginInput
+            placeholder={"Password "}
+            icon={"bi bi-eye-slash"}
+            type={"password"}
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <div className="justify-self-center">
+          <SimpleButton onClick={loginUser} />
         </div>
         <div className="justify-self-center grid text-wrap">
           <p className="mt-20">
