6e5531f36a43598b4eb152d9b8bf518ed3733f69
Hamzaali565 <ha6153806@gmail.com>
2024-06-10 12:27:31 +0500

    MR Creation Completed

--- src/Screens/IPD/Patient/PatientRegistration/PatientRegistration.jsx
+++ src/Screens/IPD/Patient/PatientRegistration/PatientRegistration.jsx
@@ -6,15 +6,21 @@ import SimpleDropDown from "../../../../Components/SimpleDropdown/SimpleDropDown
 import LabelledDropDown from "../../../../Components/LabelledDropDown/LabelledDropDown";
 import LabeledInput from "../../../../Components/LabelledInput/LabeledInput";
 import SimpleButton from "../../../../Components/Button/SimpleButton";
+import { ErrorAlert, SuccessAlert } from "../../../../Components/Alert/Alert";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const PatientRegistration = () => {
   const [patientName, setPatientName] = useState("");
   const [patientType, setPatientType] = useState([]);
   const [patientStatus, setParientStatus] = useState("");
   const [relativeName, setRelativeName] = useState("");
+  const [relativeStatus, setRelativeStatus] = useState("");
   const [fatherType, setFatherType] = useState([]);
   const [maritalStatus, setMaritalStatus] = useState([]);
+  const [maritalData, setMaritalData] = useState("");
   const [gender, setGender] = useState([]);
+  const [gendarData, setGenderData] = useState("");
   const [year, setYear] = useState("");
   const [month, setMonth] = useState("");
   const [day, setDay] = useState("");
@@ -29,6 +35,7 @@ const PatientRegistration = () => {
   const [kinCnic, setKinCnic] = useState("");
   const [kinAddress, setKinAddress] = useState("");
   const [kinOccupation, setKinOccupation] = useState("");
+  const [MrNo, setMrNo] = useState("");
   const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
@@ -58,6 +65,9 @@ const PatientRegistration = () => {
     ]);
     setGender([{ name: "--" }, { name: "Male" }, { name: "Female" }]);
   }, [toggle]);
+
+  const url = useSelector((item) => item.url);
+  const userData = useSelector((item) => item.response);
 
   //   Functions
   const patientNameF = async (name, key) => {
@@ -104,9 +114,81 @@ const PatientRegistration = () => {
     setKinCnic("");
     setKinOccupation("");
     setKinRelation("");
+    setParientStatus("");
     setToggle(!toggle);
   };
 
+  const DropDownChange = (name) => {
+    setParientStatus(name);
+    if (
+      name === "Mr." ||
+      name === "Dr." ||
+      name === "Prof" ||
+      name === "Master"
+    ) {
+      setRelativeStatus("S/o");
+      setGenderData("Male");
+      setMaritalData("Single");
+      return;
+    } else if (name === "Mrs.") {
+      setRelativeStatus("W/o");
+      setGenderData("Female");
+      setMaritalData("Married");
+      return;
+    } else if (name === "Miss") {
+      setRelativeStatus("D/o");
+      setGenderData("Female");
+      setMaritalData("Single");
+      return;
+    }
+  };
+
+  // api
+  const submitHandler = async () => {
+    try {
+      const response = await axios.post(
+        `${url}/patientreg`,
+        {
+          patientType: patientStatus,
+          MrNo,
+          patientName,
+          ageYear: year,
+          ageDay: day,
+          ageMonth: month,
+          relativeType: relativeStatus,
+          relativeName,
+          gender: gendarData,
+          occupation,
+          maritalStatus: maritalData,
+          email,
+          cellNo,
+          cnicNo: cnic,
+          address,
+          kinName,
+          kinRelation,
+          kinCell,
+          kinCnic,
+          kinAddress,
+          kinOccupation,
+          updatedUser: userData[0].userId,
+        },
+        { withCredentials: true }
+      );
+      console.log("response of Submit Handler", response);
+      Refresh();
+      if (response.data.data1) {
+        SuccessAlert({ text: "DATA UPDATED SUCCESSFULLY", timer: 2000 });
+        return;
+      }
+      SuccessAlert({
+        text: `Data Created Successfully WITH MR NO. ${response.data.data.MrNo} `,
+        timer: 2000,
+      });
+    } catch (error) {
+      console.log("Error of submit handler", error);
+      ErrorAlert({ text: error.response.data.message });
+    }
+  };
   return (
     <div>
       <div className="bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-30 shadow-lg my-4 mx-4  p-3 rounded-3xl">
@@ -118,7 +200,7 @@ const PatientRegistration = () => {
               label={"Patient Name"}
               data={patientType}
               placeholder={"Patient Name"}
-              onChangeDrop={(name) => setParientStatus(name)}
+              onChangeDrop={DropDownChange}
               value={patientName}
               onChange={(e) => {
                 patientNameF(e.target.value, "patientName");
@@ -154,7 +236,7 @@ const PatientRegistration = () => {
                     : fatherType
                   : fatherType
               }
-              onChangeDrop={(name) => console.log(name)}
+              onChangeDrop={(name) => setRelativeStatus(name)}
               value={relativeName}
               placeholder={"Relative Name"}
               onChange={(e) => {
@@ -163,8 +245,19 @@ const PatientRegistration = () => {
             />
             <LabelledDropDown
               label={"Gender"}
-              data={gender}
-              onChange={(name) => console.log(name)}
+              data={
+                patientStatus !== ""
+                  ? patientStatus === "Mr." ||
+                    patientStatus === "Dr." ||
+                    patientStatus === "Prof" ||
+                    patientStatus === "Master"
+                    ? [{ name: "Male" }]
+                    : patientStatus === "Miss" || patientStatus === "Mrs."
+                    ? [{ name: "Female" }]
+                    : gender
+                  : gender
+              }
+              onChange={(name) => setGenderData(name)}
             />
             <LabeledInput
               label={"Occupation"}
@@ -174,8 +267,21 @@ const PatientRegistration = () => {
             />
             <LabelledDropDown
               label={"Marital Status"}
-              data={maritalStatus}
-              onChange={(name) => console.log(name)}
+              data={
+                patientStatus !== ""
+                  ? patientStatus === "Mr." ||
+                    patientStatus === "Dr." ||
+                    patientStatus === "Prof" ||
+                    patientStatus === "Miss" ||
+                    patientStatus === "Master" ||
+                    patientStatus === "Baby of"
+                    ? [{ name: "Single" }]
+                    : patientStatus === "Mrs."
+                    ? [{ name: "Married" }]
+                    : gender
+                  : gender
+              }
+              onChange={(name) => setMaritalData(name)}
             />
             <LabeledInput
               label={"Email"}
@@ -195,7 +301,7 @@ const PatientRegistration = () => {
             <LabeledInput
               label={"CNIC No"}
               type={"number"}
-              placeholder={"00000-0000000-0"}
+              placeholder={"0000000000000"}
               onChange={(e) => setCnic(e.target.value)}
               value={cnic}
             />
@@ -261,9 +367,9 @@ const PatientRegistration = () => {
         </div>
       </div>
       <div className="bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-30 shadow-lg my-4 mx-4  p-3 rounded-3xl grid grid-cols-[5rem,5rem,5rem] justify-center gap-x-2">
-        <SimpleButton title={"Save"} onClick={Refresh} />
+        <SimpleButton title={"Save"} onClick={submitHandler} />
         <SimpleButton title={"Print"} />
-        <SimpleButton title={"Refresh"} />
+        <SimpleButton title={"Refresh"} onClick={Refresh} />
       </div>
     </div>
   );
