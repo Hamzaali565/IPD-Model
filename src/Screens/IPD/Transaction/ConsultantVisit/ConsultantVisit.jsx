1fda8a44eb2caee589f48846dc11aa3a821ac85d
Hamzaali565 <ha6153806@gmail.com>
2024-06-22 13:21:40 +0500

    work remaining on consultant visist page

--- src/Screens/IPD/Transaction/ConsultantVisit/ConsultantVisit.jsx
+++ src/Screens/IPD/Transaction/ConsultantVisit/ConsultantVisit.jsx
@@ -4,41 +4,102 @@ import BasicModal from "../../../../Components/Modal/FullScreenModal";
 import ConsultantModal from "../../../../Components/Modal/ConsultantModal";
 import LabeledInput from "../../../../Components/LabelledInput/LabeledInput";
 import ButtonDis from "../../../../Components/Button/ButtonDis";
+import AdmissionModal from "../../../../Components/Modal/AdmissionModal";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { ErrorAlert } from "../../../../Components/Alert/Alert";
 
 const ConsultantVisit = () => {
   const [mrInfo, setMrInfo] = useState(null);
+  const [consultant, setConsultant] = useState(null);
+  const [visiDetails, setVisistDetails] = useState([]);
+  const [date, setDate] = useState("");
+  const [remarks, setRemarks] = useState("");
+
+  const url = useSelector((state) => state.url);
+  const userId = useSelector((state) => state.response);
+
+  const checkItOut = async (data) => {
+    try {
+      setConsultant(data);
+      if (mrInfo === null) {
+        ErrorAlert({
+          text: "PLEASE SELECT ADMISSION NUMBER FIRST!!!",
+          timer: 1500,
+        });
+        return;
+      }
+      console.log("consultant", consultant);
+      const response = await axios.get(
+        `${url}/admissionconsultant?admissionNo=${mrInfo.admissionNo}&consultantId=${data?._id}`,
+        { withCredentials: true }
+      );
+      console.log(response.data.data);
+      setVisistDetails(response.data.data);
+    } catch (error) {
+      console.log("Error of CheckItOut", error);
+    }
+  };
+
+  const updateCharges = (value) => {
+    const newData = visiDetails.map((item) => {
+      return { ...item, charges: +value };
+    });
+    setVisistDetails(newData);
+  };
   return (
     <div>
       <div className="bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-30 shadow-lg my-4 mx-4  p-3 rounded-3xl">
         <CenterHeading title={"Consultant Visit"} />
         <div className="flex justify-center items-center my-4">
-          <BasicModal
+          <AdmissionModal
             title={"Select Admission No."}
             onClick={(e) => setMrInfo(e)}
           />
         </div>
-        <div className="md:flex md:justify-center md:space-x-4">
-          <p className="text-center">
-            <span className="font-bold">Patient Name:</span> Mr. FAIZANUDDIN S/o
-            WASIUDDIN
-          </p>
-          <p className="text-center">
-            {" "}
-            <span className="font-bold">Admission No:</span> 00003
-          </p>
-        </div>
+        {mrInfo !== null && (
+          <div className="md:flex md:justify-center md:space-x-4">
+            <p className="text-center">
+              <span className="font-bold">Patient Name:</span>{" "}
+              {mrInfo.patientType} {mrInfo.patientName}
+              {mrInfo.relativeType} {mrInfo.relativeName}
+            </p>
+            <p className="text-center">
+              {" "}
+              <span className="font-bold">Admission No:</span>{" "}
+              {mrInfo?.admissionNo}
+            </p>
+          </div>
+        )}
       </div>
       <div className="bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-30 shadow-lg my-4 mx-4  p-3 rounded-3xl">
         <CenterHeading title={"Consultant Visit Details"} />
 
         <div className="flex flex-col items-center space-y-2 ">
           <div className="flex items-center">
-            <ConsultantModal title={"Select Consultant"} />
-            <p>DR. ASAD ALI KERAWALA</p>
+            <ConsultantModal
+              title={"Select Consultant"}
+              onClick={(e) => checkItOut(e)}
+            />
+            {consultant !== null && <p>{consultant?.name}</p>}
           </div>
-          <LabeledInput label={"Charges"} placeholder={"Consultant Charges"} />
-          <LabeledInput label={"Visit Date"} type={"date"} />
-          <LabeledInput label={"Remarks"} placeholder={"Remarks"} />
+          <LabeledInput
+            label={"Charges"}
+            placeholder={"Consultant Charges"}
+            type={"Number"}
+            value={visiDetails.length > 0 ? visiDetails[0].charges : ""}
+            onChange={(e) => updateCharges(e.target.value)}
+          />
+          <LabeledInput
+            label={"Visit Date"}
+            type={"date"}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <LabeledInput
+            label={"Remarks"}
+            placeholder={"Remarks"}
+            onChange={(e) => setRemarks(e.target.value)}
+          />
           <ButtonDis title={"Save"} />
         </div>
 
