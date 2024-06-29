f9484d0c72f14b4fd3c78c578baf3a576f10070e
Hamzaali565 <ha6153806@gmail.com>
2024-06-29 11:58:15 +0500

    Payment model completed

--- src/Components/SimpleInput/SimpleInput.jsx
+++ src/Components/SimpleInput/SimpleInput.jsx
@@ -5,7 +5,8 @@ const SimpleInput = ({ max, placeholder, type, onChange, value, disabled }) => {
     <div>
       <input
         type={type}
-        className="text-sm w-40 rounded-xl p-1 border-2 border-gray-500 text-black"
+        className="text-sm w-44 rounded-xl p-1 border-2 border-gray-500 text-black"
+        style={{ fontSize: "12px" }}
         placeholder={placeholder}
         onChange={onChange}
         value={value}
--- src/Screens/IPD/Payments/PaymentReciept/PaymentReciept.jsx
+++ src/Screens/IPD/Payments/PaymentReciept/PaymentReciept.jsx
@@ -1,22 +1,109 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CenterHeading from "../../../../Components/Center Heading/CenterHeading";
 import SimpleDropDown from "../../../../Components/SimpleDropdown/SimpleDropDown";
 import AdmissionModal from "../../../../Components/Modal/AdmissionModal";
 import SimpleInput from "../../../../Components/SimpleInput/SimpleInput";
 import LabeledInput from "../../../../Components/LabelledInput/LabeledInput";
+import ButtonDis from "../../../../Components/Button/ButtonDis";
+import { ErrorAlert, SuccessAlert } from "../../../../Components/Alert/Alert";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const PaymentReciept = () => {
   const [paymentType, setPaymentType] = useState("");
   const [location, setLocation] = useState("");
   const [paymentAgainst, setPaymentAgaisnt] = useState("");
-  const [amount, setAmount] = useState("");
+  const [amount, setAmount] = useState(null);
+  const [mrInfo, setMrInfo] = useState(null);
+  const [remarks, setRemarks] = useState("");
+  const [paymentAgainstData, setPaymentAgainstData] = useState([]);
+  const [paymentTypeData, setPaymentTypeData] = useState([]);
+  const [locationData, setLocationData] = useState([]);
+  const [toggle, setToggle] = useState(false);
 
   const userData = useSelector((state) => state.response);
   const url = useSelector((state) => state.url);
   let shiftData = useSelector((state) => state.shift);
 
   console.log(shiftData);
+
+  useEffect(() => {
+    setPaymentTypeData([
+      { name: "--" },
+      { name: "Cash" },
+      { name: "Credit Card" },
+      { name: "Online" },
+      { name: "Cheque" },
+      { name: "Pay Order" },
+    ]);
+
+    setPaymentAgainstData([
+      { name: "--" },
+      { name: "Advance Admission" },
+      { name: "Advance ER" },
+    ]);
+
+    setLocationData([
+      { name: "--" },
+      { name: "Main Reception" },
+      { name: "OPD Recetion" },
+    ]);
+  }, [toggle]);
+
+  //function
+  const resetData = () => {
+    setPaymentType("");
+    setLocation("");
+    setLocationData([]);
+    setPaymentAgainstData([]);
+    setPaymentTypeData([]);
+    setPaymentAgaisnt("");
+    setAmount(null);
+    setRemarks("");
+    setMrInfo(null);
+
+    setToggle(!toggle);
+  };
+
+  const checkValidation = () => {
+    try {
+      if (!paymentAgainst) throw new Error("PLEASE SELECT PAYMENT AGAINST !!!");
+      if (!paymentType) throw new Error("PLEASE SELECT PAYMENT TYPE !!!");
+      if (!location) throw new Error("PLEASE SELECT LOCATION !!!");
+      if (mrInfo === null) throw new Error("PLEASE SELECT ADMISSION NO.");
+      if (!amount) throw new Error("PLEASE ENTER AMOUNT !!!");
+      submitPayment();
+    } catch (error) {
+      ErrorAlert({ text: error.message, timer: 2000 });
+    }
+  };
+
+  // api
+  const submitPayment = async () => {
+    try {
+      const response = await axios.post(
+        `${url}/paymentreciept`,
+        {
+          paymentType,
+          location,
+          paymentAgainst,
+          amount,
+          shiftNo: shiftData[0].ShiftNo,
+          againstNo: mrInfo?.admissionNo,
+          mrNo: mrInfo?.mrNo,
+          remarks,
+          createdUser: userData[0]?.userId,
+        },
+        { withCredentials: true }
+      );
+      console.log("response of submitPayment", response.data.data);
+      SuccessAlert({ text: "PAYMENT CREATED SUCCESSFULLY !!!", timer: 2000 });
+      resetData();
+    } catch (error) {
+      console.log("Error of submitPayment", error.response);
+      ErrorAlert({ text: "SOMETHING WENT WRONG !!!", timer: 2000 });
+    }
+  };
 
   return (
     <div>
@@ -25,36 +112,26 @@ const PaymentReciept = () => {
         <div className="flex flex-col items-center space-y-2">
           <SimpleDropDown
             DropDownLabel={"Payment Against"}
-            data={[
-              { name: "--" },
-              { name: "Advance Admission" },
-              { name: "Advance ER" },
-            ]}
+            data={paymentAgainstData}
             onChange={(e) => setPaymentAgaisnt(e)}
           />
           <SimpleDropDown
             DropDownLabel={"Payment Type"}
-            data={[
-              { name: "--" },
-              { name: "Cash" },
-              { name: "Credit Card" },
-              { name: "Online" },
-              { name: "Cheque" },
-              { name: "Pay Order" },
-            ]}
+            data={paymentTypeData}
+            onChange={(e) => setPaymentType(e)}
           />
           <SimpleDropDown
             DropDownLabel={"Collecting Location"}
-            data={[
-              { name: "--" },
-              { name: "Main Reception" },
-              { name: "OPD Recetion" },
-            ]}
+            data={locationData}
+            onChange={(e) => setLocation(e)}
           />
           {paymentAgainst === "Advance Admission" && (
             <div className="flex items-center flex-col space-y-2">
               {paymentAgainst === "Advance Admission" ? (
-                <AdmissionModal title={"Select AdmissionNo."} />
+                <AdmissionModal
+                  title={"Select AdmissionNo."}
+                  onClick={(e) => setMrInfo(e)}
+                />
               ) : (
                 ""
               )}
@@ -63,16 +140,23 @@ const PaymentReciept = () => {
                 disabled={true}
                 placeholder={"Patient Name"}
                 label={"Patient Name"}
+                value={
+                  mrInfo !== null
+                    ? `${mrInfo.patientType} ${mrInfo.patientName}  ${mrInfo.relativeType} ${mrInfo.relativeName}`
+                    : ""
+                }
               />
               <LabeledInput
                 disabled={true}
                 placeholder={"Admission No"}
                 label={"Admission No"}
+                value={mrInfo !== null ? mrInfo?.admissionNo : ""}
               />
               <LabeledInput
                 disabled={true}
                 placeholder={"Mr No"}
                 label={"Mr No"}
+                value={mrInfo !== null ? mrInfo?.mrNo : ""}
               />
             </div>
           )}
@@ -80,8 +164,19 @@ const PaymentReciept = () => {
             type={"Number"}
             placeholder={"Amount"}
             label={"Amount"}
+            value={amount === null ? "" : amount}
+            onChange={(e) => setAmount(+e.target.value)}
           />
-          <LabeledInput placeholder={"Remarks"} label={"Remarks"} />
+          <LabeledInput
+            placeholder={"Remarks"}
+            label={"Remarks"}
+            value={remarks}
+            onChange={(e) => setRemarks(e.target.value.toUpperCase())}
+          />
+          <div className="flex items-center space-x-2">
+            <ButtonDis title={"Save"} onClick={checkValidation} />
+            <ButtonDis title={"Refresh"} onClick={resetData} />
+          </div>
         </div>
       </div>
     </div>
