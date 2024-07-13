a6eca8bc64f6514b26f5799ae72d0f914def1331
Hamzaali565 <ha6153806@gmail.com>
2024-07-13 12:02:47 +0500

    working on refund of radiology

--- src/Screens/PortScreen/Port.jsx
+++ src/Screens/PortScreen/Port.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import logo from "../../Images/ZMCLogo-2.png";
 const Port = () => {
-  return <div>Welcome to FIQOTECH</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <img src={logo} alt="" />
+    </div>
+  );
 };
 
 export default Port;
