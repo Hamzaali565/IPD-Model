cf7205a9182c21f83446fc3f1e4c7ad5dac15694
Hamzaali565 <ha6153806@gmail.com>
2024-08-07 02:07:22 +0500

    update of office work

--- src/Screens/LAB/MASTER/LabCharges.jsx
+++ src/Screens/LAB/MASTER/LabCharges.jsx
@@ -4,6 +4,8 @@ import PartyModal from "../../../Components/Modal/PartyModal";
 import axios from "axios";
 import Loader from "../../../Components/Modal/Loader";
 import { useSelector } from "react-redux";
+import ButtonDis from "../../../Components/Button/ButtonDis";
+import { ErrorAlert, SuccessAlert } from "../../../Components/Alert/Alert";
 
 const LabCharges = () => {
   const [party, setParty] = useState(null);
@@ -12,9 +14,14 @@ const LabCharges = () => {
 
   const url = useSelector((item) => item?.url);
 
+  // reset page
+  const reset = () => {
+    setChargesDetails([]);
+    setParty(null);
+  };
+
+  // get previous charges
   const getChargesDetails = async (data) => {
-    console.log(data);
-    
     try {
       setOpen(true);
       setParty(data);
@@ -28,6 +35,62 @@ const LabCharges = () => {
     } catch (error) {
       console.log("Error of getChargesDetails", error);
       setOpen(false);
+    }
+  };
+
+  // handle set value charges details
+  const handlerEffect = (charges, id, value) => {
+    const updateChargesDetails = chargesDetails.map((items) => {
+      if (items?.testId === id) {
+        if (value === "charges") {
+          return { ...items, charges: +charges };
+        } else if (value === "status") {
+          return { ...items, status: charges };
+        }
+      }
+      return items;
+    });
+    setChargesDetails(updateChargesDetails);
+  };
+
+  const CheckValidation =  () => {
+    try {
+      if (party === null) {
+        throw new Error("PLEASE SELECT PARTY FIRST !!!");
+      } else if (chargesDetails?.length <= 0) {
+        throw new Error("PLEASE ENTER VALUE IN CHARGES DETAILS !!!");
+      }
+       submitData()
+    } catch (error) {
+      ErrorAlert({ text: error?.message, timer: 2000 });
+    }
+  };
+
+  // Submit data
+  const submitData = async () => {
+    try {
+      setOpen(true);
+      const response = await axios.post(
+        `${url}/lab/testsChargesPush`,
+        {
+          partyName: party?.name,
+          partyId: party?._id,
+          labDetails: chargesDetails,
+        },
+        { withCredentials: true }
+      );
+      setOpen(false);
+      SuccessAlert({
+        text: `LAB CHARGES CREATED/UPDATED AGAINST PARTY ${party?.name}`,
+        timer: 1000,
+      });
+      console.log(response);
+
+      reset();
+    } catch (error) {
+      setOpen(false);
+      console.log("Error of submit Data ", error);
+      setChargesDetails([])
     }
   };
 
@@ -61,13 +124,29 @@ const LabCharges = () => {
                 <div className="grid grid-cols-4 text-xs justify-items-center items-center h-10 border border-gray-300">
                   <p className="">{items?.testCode}</p>
                   <p className="">{items?.testName}</p>
-                  <p className="">{items?.charges}</p>
+                  <p className="">
+                    <input
+                      type="number"
+                      className="w-24 rounded-xl p-1"
+                      placeholder="Charges"
+                      min={0}
+                      name=""
+                      value={items?.charges}
+                      onChange={(e) =>
+                        handlerEffect(e.target.value, items?.testId, "charges")
+                      }
+                      id=""
+                    />
+                  </p>
                   <p className="">
                     <input
                       type="checkbox"
                       name=""
                       id=""
-                      value={items?.status}
+                      checked={items?.status}
+                      onChange={(e) =>
+                        handlerEffect(e.target.checked, items?.testId, "status")
+                      }
                     />
                   </p>
                 </div>
@@ -75,6 +154,10 @@ const LabCharges = () => {
             ))}
           </div>
         )}
+      </div>
+      <div className="flex flex-col items-center space-y-2 md:flex-row md:space-y-0 md:space-x-2 justify-center">
+        <ButtonDis title={"Save"} onClick={CheckValidation} />
+        <ButtonDis title={"Refresh"} onClick={reset} />
       </div>
       <Loader onClick={open} title={"Please Wait ..."} />
     </div>
